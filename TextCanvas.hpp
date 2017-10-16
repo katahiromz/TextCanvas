b8b15b71c2e53bd3cee287c87d517771954fac86
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-10-16 18:31:31 +0900

    trivial 2

--- TextCanvas.hpp
+++ TextCanvas.hpp
@@ -81,7 +81,7 @@ namespace textcanvas
     {
         return x0 <= x && x <= x1 && y0 <= y && y <= y1;
     }
-    
+
     bool get_polygon_box(Point& p0, Point& p1, size_t num_points, const Point *points);
 
     size_t crossing_number(Point p, const Points& points);
