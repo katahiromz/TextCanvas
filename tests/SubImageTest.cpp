b5172f46976b216b43b1412cdac0a351fb691306
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-10-17 08:58:17 +0900

    fix SubImageTest

--- tests/SubImageTest.cpp
+++ tests/SubImageTest.cpp
@@ -6,7 +6,7 @@ int main(void)
     textcanvas::TextCanvas canvas(100, 100), subimage;
     canvas.rectangle(10, 10, 25, 25);
     canvas.line(10, 10, 25, 25);
-    canvas.line(10, 25, 10, 25);
+    canvas.line(25, 10, 10, 25);
     canvas.get_subimage(subimage, 10, 10, 25, 25);
     canvas.put_subimage(35, 35, subimage, 2, 3);
     std::cout << canvas.to_str();
