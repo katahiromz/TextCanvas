fed31448f2e9242e1808ca1823f8ea51195e1719
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-11-03 13:13:29 +0900

    improve

--- tests/TextOutTest.cpp
+++ tests/TextOutTest.cpp
@@ -4,7 +4,7 @@
 int main(void)
 {
     using namespace textcanvas;
-    TextCanvas canvas(8 * 9, 16 * 2);
+    TextCanvas canvas(8 * 9, 16 * 2, '#', ' ');
     canvas.text_to_right(0, 0, kh_dot_hankaku_font(), "ABCabc123");
     //canvas.jis_put_char(0, 16, kh_dot_zenkaku_font(), japanese::sjis2jis(0x93FA));
     canvas.sjis_text_to_right(0, 16, kh_dot_zenkaku_font(), kh_dot_hankaku_font(), "\x93\xFA\x96\x7B\x8C\xEA\xB1\xB2" "AB"); // Shift_JIS: “ú–{Œê±²AB
